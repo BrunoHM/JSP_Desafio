722b935de993bd808a101d8c1c03553ad9405bee
Bruno Murta <brunoh.murta@gmail.com>
2020-08-08 20:13:35 -0300

    Implementado método para buscar todos usuários cadastrados.

--- src/dao/UsuarioDao.java
+++ src/dao/UsuarioDao.java
@@ -1,5 +1,40 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import utils.DtbUtil;
+
 public class UsuarioDao {
 
+	private Connection conexaoDtb = new DtbUtil().getConexao();
+	private PreparedStatement queryDtb;
+	private ResultSet resultadoDtb;
+	
+	private void insertUsuario() {
+		try {
+			queryDtb = conexaoDtb.prepareStatement("INSERT INTO cliente (nome, sobrenome, cpf, ativo) VALUES (?,?,?,?)");
+			//Implementar Insert Usuario ...		
+		}catch(Exception e) {
+			
+		}
+	}
+	
+	public ResultSet getAllusuario() {
+		selectAllUsuarios();
+		return resultadoDtb;
+	}
+	
+	private void selectAllUsuarios() {
+		try {
+			queryDtb = conexaoDtb.prepareStatement("select * from usuario");
+			queryDtb.execute();
+			resultadoDtb = queryDtb.getResultSet();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
 }
