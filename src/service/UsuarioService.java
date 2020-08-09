271a311e1bc9fbdaaf48e562430afb7aede2d4ee
Bruno Murta <brunoh.murta@gmail.com>
2020-08-09 03:21:50 -0300

    Adicionado métodos para deletar usuário na base de dados e método que verifica ação solicitada no front-end

--- src/service/UsuarioService.java
+++ src/service/UsuarioService.java
@@ -14,9 +14,32 @@ public class UsuarioService {
 	ResultSet retornoSelect;
 	
 	UsuarioDao usuarioDao = new UsuarioDao();
-	Usuario usuario = new Usuario();
+	Usuario usuario       = new Usuario();
 	
-	public void getTodosUsuarios(HttpServletRequest request) {
+	
+	public void checkAction(HttpServletRequest request) {
+		String requestUri = request.getRequestURI();
+		requestUri = requestUri.substring(requestUri.lastIndexOf("/")+1);
+		
+		if(requestUri.equals("usuarios")) {
+			getTodosUsuarios(request);
+		}else if(requestUri.equals("deletaUsuario")) {
+			deletaUsuario(request);
+		}
+		
+	}
+	
+	private void deletaUsuario(HttpServletRequest request) {
+		if(request.getParameter("idUserDel") != null) {
+			int idUsuario = Integer.valueOf(request.getParameter("idUserDel"));
+			if(idUsuario > 0) {
+				usuarioDao.deletaUsuario(idUsuario);
+			}
+		}
+		
+	}
+
+	private void getTodosUsuarios(HttpServletRequest request) {
 		try {
 			retornoSelect = usuarioDao.getAllusuarios();
 			List<Usuario> listUsuario = new ArrayList<Usuario>();
@@ -38,5 +61,7 @@ public class UsuarioService {
 			System.err.println("Não foi possível retornar os usuários cadastrados na base de dados!");
 		}
 	}
+	
+	
 	
 }
