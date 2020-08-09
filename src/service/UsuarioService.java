ddbe6a32662faccb66f246aa4b12214cce471855
Bruno Murta <brunoh.murta@gmail.com>
2020-08-08 21:41:59 -0300

    Adicionado método para retornar todos usuários cadastros.

--- src/service/UsuarioService.java
+++ src/service/UsuarioService.java
@@ -1,5 +1,42 @@
 package service;
 
-public class UsuarioService {
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
+import dao.UsuarioDao;
+import model.Usuario;
+
+public class UsuarioService {
+	
+	ResultSet retornoSelect;
+	
+	UsuarioDao usuarioDao = new UsuarioDao();
+	Usuario usuario = new Usuario();
+	
+	public void getTodosUsuarios(HttpServletRequest request) {
+		try {
+			retornoSelect = usuarioDao.getAllusuarios();
+			List<Usuario> listUsuario = new ArrayList<Usuario>();
+			
+			while(retornoSelect.next()) {
+				usuario = new Usuario();
+				
+				usuario.setId(retornoSelect.getInt(1));
+				usuario.setNome(retornoSelect.getString(2));
+				usuario.setSobrenome(retornoSelect.getString(3));
+				usuario.setCpf(retornoSelect.getString(4));
+				usuario.setAtivo(retornoSelect.getBoolean(5));
+				
+				listUsuario.add(usuario);
+			}
+			request.setAttribute("queryResults",listUsuario);
+			
+		}catch(Exception e) {
+			System.err.println("Não foi possível retornar os usuários cadastrados na base de dados!");
+		}
+	}
+	
 }
