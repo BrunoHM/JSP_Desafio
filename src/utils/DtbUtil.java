04ca38495d951a3b296b9ea28153b49f5e1fb8a5
Bruno Murta <brunoh.murta@gmail.com>
2020-08-10 02:16:56 -0300

    Alterado String de conexão com mysql para facilitar edição

--- src/utils/DtbUtil.java
+++ src/utils/DtbUtil.java
@@ -6,12 +6,16 @@ import java.sql.DriverManager;
 public class DtbUtil {
 
 	public static Connection conexaoDtb;
+	private static final String conexao = "jdbc:mysql://localhost:3306/desafio_Jsp?serverTimezone=UTC";
+	private static final String usuario = "root";
+	private static final String senha = "@Grilo1324";
+
 	
 	public static Connection getConexao() {
 		if(conexaoDtb == null) {
 			try {
 				Class.forName("com.mysql.cj.jdbc.Driver");
-				conexaoDtb = DriverManager.getConnection("jdbc:mysql://localhost:3306/desafio_Jsp?serverTimezone=UTC","root","@Grilo1324");
+				conexaoDtb = DriverManager.getConnection(conexao, usuario, senha);
 			}catch (Exception e) {
 				e.printStackTrace();
 			}
