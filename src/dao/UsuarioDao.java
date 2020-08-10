c0407d8ae1fa265d78c7edd7bb4136a952b7bf52
Bruno Murta <brunoh.murta@gmail.com>
2020-08-09 23:16:27 -0300

    Adicionado e Alterado diversas implementações

--- src/dao/UsuarioDao.java
+++ src/dao/UsuarioDao.java
@@ -3,7 +3,11 @@ package dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import model.Usuario;
 import utils.DtbUtil;
 
 public class UsuarioDao {
@@ -12,14 +16,7 @@ public class UsuarioDao {
 	private PreparedStatement queryDtb;
 	private ResultSet resultadoDtb;
 	
-//	private void insertUsuario() {
-//		try {
-//			queryDtb = conexaoDtb.prepareStatement("INSERT INTO cliente (nome, sobrenome, cpf, ativo) VALUES (?,?,?,?)");
-//			Implementar Insert Usuario ...		
-//		}catch(Exception e) {
-//			
-//		}
-//	}
+	private Long idUsuario;
 	
 	public ResultSet getAllusuarios() {
 		selectAllUsuarios();
@@ -35,6 +32,7 @@ public class UsuarioDao {
 			e.printStackTrace();
 		}
 	}
+	
 	
 	public void deletaUsuario(int idUsuario) {
 		deleteUser(idUsuario);
@@ -53,6 +51,40 @@ public class UsuarioDao {
 		
 		return statusOperation;
 	}
+	
+	public void addUsuario(Usuario usuario) {
+		insertUsuario(usuario);
+	}
+	
+	private void insertUsuario(Usuario usuario) {
+		try {
+			queryDtb = conexaoDtb.prepareStatement("INSERT INTO usuario (nome, sobrenome, cpf, ativo) VALUES (?,?,?,?)", queryDtb.RETURN_GENERATED_KEYS);
+			
+			queryDtb.setString(1, usuario.getNome());
+			queryDtb.setString(2, usuario.getSobrenome());
+			queryDtb.setString(3, usuario.getCpf());
+			queryDtb.setBoolean(4, usuario.getAtivo());
+			
+			queryDtb.executeUpdate();
+
+			try (ResultSet generatedKeys = queryDtb.getGeneratedKeys()) {
+				
+				if (generatedKeys.next()) {
+					idUsuario = generatedKeys.getLong(1);
+	            }else {
+	                throw new SQLException("Erro ao recuperar código de novo usuário");
+	            }
+	        }
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Long getIdUsuario() {
+		return idUsuario;
+	}
+	
 	
 	
 }
